77e0eb3a591d0a9b0250acbc1c2e0ddaefc57ebe
Saracen <SaracenOne@gmail.com>
2023-10-02 15:18:37 +0100

    Add XBot model

--- net_demo/core/character_model_holder.gd
+++ net_demo/core/character_model_holder.gd
@@ -1,9 +1,9 @@
 extends Node3D
 
 var color_material: Material = null
+@export var character_mesh_instance: MeshInstance3D = null
 
 func assign_multiplayer_material(p_material: Material) -> void:
 	color_material = p_material
 	
-	$ThirdPersonModel/Base.material_override = color_material
-	$ThirdPersonModel/Pointer.material_override = color_material
+	character_mesh_instance.material_override = color_material
